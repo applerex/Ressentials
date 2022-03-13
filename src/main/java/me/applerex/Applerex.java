66f1b1f09a50d2630872bff2180b98cb189beace
DaBlueHat <101466376+applerex@users.noreply.github.com>
2022-03-13 08:02:34 +0000

    Basics

--- src/main/java/me/applerex/Applerex.java
+++ src/main/java/me/applerex/Applerex.java
@@ -1,17 +1,19 @@
 package me.applerex;
 
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Applerex extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN + "[Resentials] plugin has started");
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getServer().getConsoleSender().sendMessage(ChatColor.RED + "[Resentials] plugin has stopped, you may need to download LuckPerms (only if you dont have it already).");
     }
 }
